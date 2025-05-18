2629ec46a319ef82854ebc5f375504d7c30cfcfb
cfevw <67218238+cfevw@users.noreply.github.com>
2025-05-18 11:07:42 +0000

    Deploy

--- app/links/page.tsx
+++ app/links/page.tsx
@@ -19,15 +19,15 @@ export default function About() {
   return (
     <div className="text-center">
       <h1 className="text-3xl m-3">cfevw's Links</h1>
-      <table className="text-center">
+      <table className="text-center m-auto b-w">
         <tbody>
           {LINKS.map((link, i) => {
-            return (<tr key={i}><td>{link.name}</td><td><a href={link.identity_url}>{link.identity}</a></td></tr>);
+            return (<tr key={i} className="border-b border-gray-600"><td className="py-3 px-6 bg-gray-700 text-white uppercase text-sm leading-normal">{link.name}</td><td className="py-3 px-6 hover:bg-gray-600 underline"><a href={link.identity_url}>{link.identity}</a></td></tr>);
           })}
         </tbody>
       </table>
       <div>
-        Back to <Link href="/">Home</Link>
+        Back to <Link href="/" className="underline">Home</Link>
       </div>
     </div>
   );
